241d10480fa26398d61719bd2502126f8a9e1fba
Andre Almar <andre@y7mail.com>
2016-01-13 00:19:32 -0200

    added get_timestamp endpoint

--- hello-world.py
+++ hello-world.py
@@ -5,6 +5,9 @@ from redis import Redis
 from flask import request
 from flask import jsonify
 
+#used to get the timestamp of the server
+import datetime
+
 app = Flask(__name__)
 redis = Redis(host='redis', port= 6379)
 
@@ -24,6 +27,13 @@ def hello_world():
 @app.route("/get_ip", methods=["GET"])
 def get_ip():
     return jsonify({'ip': request.remote_addr}), 200
+
+#returns unix timestamp of the request
+@app.route("/get_timestamp", methods=["GET"])
+def timestamp():
+    #global ts
+    ts = datetime.datetime.now().strftime("%A, %d. %B %Y %I:%M%p")
+    return jsonify({'timestamp': ts})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
